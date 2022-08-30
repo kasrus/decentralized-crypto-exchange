8eb8b997df9d7451fd60f259c86d7f5395241c62
Kasey <kruslim@gmail.com>
2022-08-30 15:42:55 -0700

    2. Smoke test

---
+++ scripts/1_deploy.js
@@ -0,0 +1,17 @@
+//import {ethers} from 
+async function main() {
+    //get the contract to deploy
+    const Token = await ethers.getContractFactory("Token");
+    //deploy contract
+    const token = await Token.deploy();
+    //fetch copy of the contract that was deployed
+    await token.deployed();
+    console.log(`Token deployed to: ${token.address}`);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
