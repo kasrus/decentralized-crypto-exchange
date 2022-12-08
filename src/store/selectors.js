18a0f5a608f5a6957c46957b01b8869263eeba38
Kasey <kruslim@gmail.com>
2022-12-07 21:24:55 -0800

    26. Trades

--- src/store/selectors.js
+++ src/store/selectors.js
@@ -52,7 +52,71 @@ const decorateOrder = (order, tokens) => {
     })
 }
 
-// -----------------------------
+// -------------------------------------------------------------
+// ALL FILLED ORDERS
+export const filledOrdersSelector = createSelector(
+    filledOrders,
+    tokens,
+    (orders, tokens) => {
+        if(!tokens[0] || !tokens[1]) { return } //always safeguarding
+
+        //Filter orders by selected tokens - this basically to filter 
+        //so that it only shows to the applicable trading pair
+        orders = orders.filter((o) => o.tokenGet === tokens[0].address || o.tokenGet === tokens[1].address)
+        orders = orders.filter((o) => o.tokenGive === tokens[0].address || o.tokenGive === tokens[1].address)
+
+        // [x] Step 1: sort orders by time ascending        
+        // [x] Step 2: apply order colors (decorate orders)
+        // Step 3: sort orders by time descending for UI
+
+        //Sort orders by time ascending for price comparison 
+        orders = orders.sort((a, b) => a.timestamp - b.timestamp)
+
+        //Decorate the orders
+        orders = decorateFilledOrders(orders, tokens)
+
+        //Sort orders by descending time for display
+        orders = orders.sort((a, b) => b.timestamp - a.timestamp)
+        return orders
+    }
+)
+
+const decorateFilledOrders = (orders, tokens) => {
+    //Track previous order to compare history
+    let previousOrder = orders[0] //initially the first order is the previous order
+
+    return (orders.map((order) => {
+        //decorate each individual order
+        order = decorateOrder(order, tokens)
+        order = decorateFilledOrder(order, previousOrder)
+        previousOrder = order //Update the previous order once it's decorated
+        return order
+    })
+    )
+}
+
+const decorateFilledOrder = (order, previousOrder) => {
+    return ({
+        ...order,
+        tokenPriceClass: tokenPriceClass(order.tokenPrice, order.id, previousOrder)
+    })  
+}
+
+const tokenPriceClass = (tokenPrice, orderId, previousOrder) => {
+    // Show green price if only one order exists
+    if(previousOrder.id === orderId) {
+        return GREEN
+    }
+    // Show green price if order price is higher than previous order
+    // Show red price if order price is lower than previous order
+    if(previousOrder.tokenPrice <= tokenPrice) {
+        return GREEN //success
+    } else {
+        return RED //danger
+    }
+}
+
+// -------------------------------------------------------------
 // ORDER BOOK
 //allOrders from redux store, tokens gonna be an array, callback function
 export const orderBookSelector = createSelector(
